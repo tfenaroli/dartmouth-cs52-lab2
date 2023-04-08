8a20e24c5a3696ea22a7a4b216c656aa41a2f60b
Thomas Fenaroli <tfenaroli@icloud.com>
2023-04-07 22:36:37 -0400

    basic functionality completed

--- main.js
+++ main.js
@@ -1,3 +1,57 @@
-document.getElementById("test").addEventListener("click", function (event) {
-	console.log(event);
-})
+console.log("JS is working!");
+
+$("#submit").on("click", function (e) {
+  // gather all checked radio-button values
+  var choices = $("input[type='radio']:checked")
+    .map(function (i, radio) {
+      return $(radio).val();
+    })
+    .toArray();
+
+  // now you have an array of choices = ["valueofradiobox1", "valueofradiobox2", "valueofradiobox2"]
+  // you'll need to do some calculations with this
+  // a naive approach would be to just choose the most common option - seems reasonable
+
+  // if user hasn't answered all questions
+  if (choices.length < 4) {
+    alert("Must answer all questions!");
+  }
+  // is user has answered all questions
+  else {
+    console.log(choices.toString());
+    // create a dictionary mapping from house to frequency
+    const houseToFreq = {};
+    choices.forEach((el) => {
+      if (houseToFreq[el]) {
+        houseToFreq[el] = houseToFreq[el] + 1;
+      } else {
+        houseToFreq[el] = 1;
+      }
+    });
+
+    // get the most frequent house
+    let mostFreq = Object.keys(houseToFreq)[0];
+    Object.keys(houseToFreq).forEach((el) => {
+      if (houseToFreq[el] > houseToFreq[mostFreq]) {
+        mostFreq = el;
+      }
+    });
+
+    console.log(houseToFreq);
+    console.log(mostFreq);
+    $("#submit").hide();
+    $("#result").text("You are a " + mostFreq.toUpperCase() + "!!!");
+    $("#sorting_hat").show();
+  }
+});
+
+$("input[type='radio']").on("click", function (e) {
+  const question = $(this).attr("name");
+  console.log($(this).attr("name"));
+  // checked img
+  if ($(`input[name="${question}"]:checked ~ *`).hasClass("inactive")) {
+    $(`input[name="${question}"]:checked ~ *`).removeClass("inactive");
+  }
+  // unchecked imgs
+  $(`input[name="${question}"]:not(:checked) ~ *`).addClass("inactive");
+});
